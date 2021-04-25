0c9ecd6aee9ed135071931f755a4e8a9b4038420
Viona Kaleb <vionakaleb@gmail.com>
2021-04-25 19:38:24 +0700

    Sidebar

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -13,7 +13,7 @@
     props: {
       type: {
         type: String,
-        default: 'success',
+        default: '',
         description: 'Header background type'
       }
     }
--- src/views/MockUp.vue
+++ src/views/MockUp.vue
@@ -1,19 +1,27 @@
 <template>
-  <div class="bg-gradient-warning">
-    <base-header class="pb-7 pt-2">
+  <div class="m-0 p-0">
+    <b-sidebar id="sidebar-variant" title="Sidebar" bg-variant="dark" text-variant="light" shadow>
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+          in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+        </p>
+        <b-img src="https://picsum.photos/500/500/?image=54" fluid thumbnail></b-img>
+      </div>
+    </b-sidebar>
+    <div>
+      <base-header class="pb-7 pt-2">
       <!-- Card stats -->
       <b-row class="header-h mb-2">
         <b-col xl="1" md="" sm="">
           <stats-card
             type="dark"
-            icon="ni ni-chart-pie-35"
             class="header-h p-2">
           </stats-card>
         </b-col>
         <b-col xl="8" md="" sm="">
           <stats-card
             type="dark"
-            icon="ni ni-chart-pie-35"
             class="header-h p-2">
               <h1 class="ml-2 text-uppercase">Rawat Jalan</h1>
           </stats-card>
@@ -21,7 +29,6 @@
         <b-col xl="3" md="" sm="">
           <stats-card
             type="dark"
-            icon="ni ni-chart-pie-35"
             class="header-h p-2">
           </stats-card>
         </b-col>
@@ -56,7 +63,7 @@
             type="dark"
             class="header-h m-0 p-1 text-center">
               <h5 class="m-0 text-uppercase">DPJP</h5>
-              <h1 class="m-0 text-uppercase">dr. Febri Bahari, SpPD, KGEH</h1>
+              <h1 class="m-0">dr. Febri Bahari, SpPD, KGEH</h1>
           </stats-card>
         </b-col>
         <b-col xl="3" md="" sm="">
@@ -64,7 +71,7 @@
             type="dark"
             class="header-h m-0 p-1 text-center">
               <h5 class="m-0 text-uppercase">Anda Masuk Sebagai</h5>
-              <h1 class="m-0 text-uppercase">dr. Dany Petra, SpPD</h1>
+              <h1 class="m-0">dr. Dany Petra, SpPD</h1>
           </stats-card>
         </b-col>
       </b-row>
@@ -74,7 +81,7 @@
             <b-col md="12">
               <stats-card
                 type="dark"
-                class="header-h m-0 p-1 text-center">
+                class="header-h m-0 p-1 text-left">
                   <h5 class="m-0 text-uppercase">Keluhan Utama</h5>
                   <h2 class="m-0 text-capitalize">KU: Muntah berdarah sejak 1 hari yang lalu.</h2>
               </stats-card>
@@ -84,7 +91,7 @@
             <b-col md="12">
               <stats-card
                 type="dark"
-                class="header-h m-0 p-1 text-center">
+                class="header-h m-0 p-1 text-left">
                   <h5 class="m-0 text-uppercase">Diagnosis Primer & Sekunder</h5>
                   <h2 class="m-0 text-capitalize">DP: Esophageal varices with bleeding (l85.01) ...</h2>
               </stats-card>
@@ -96,29 +103,44 @@
             type="dark"
             class="header2-h">
             <b-row>
-              <b-col class="text-uppercase">Tanda-Tanda Vital</b-col>
-              <b-col>H+3</b-col>
-              <b-col>Last Update: SELASA, 27 OKT 2020, 06.30 WIB.</b-col>
+              <b-col md="4" class="text-uppercase">Tanda-Tanda Vital</b-col>
+              <b-col md="2">H+3</b-col>
+              <b-col md="6">Last Update: SELASA, 27 OKT 2020, 06.30 WIB.</b-col>
             </b-row>
             <b-row>
-              <b-col></b-col>
-              <b-col></b-col>
-              <b-col></b-col>
-              <b-col></b-col>
-              <b-col></b-col>
-              <b-col></b-col>
+              <b-col md="2">
+                <div class="bg-success" style="border-radius: 5px; height: 6vh;">Kesadaran</div>
+              </b-col>
+              <b-col md="2">
+                <div class="bg-success" style="border-radius: 5px; height: 6vh;">Tekanan Darah</div>
+              </b-col>
+              <b-col md="2">
+                <div class="bg-danger" style="border-radius: 5px; height: 6vh;">Laju Nadi</div>
+              </b-col>
+              <b-col md="2">
+                <div class="bg-warning" style="border-radius: 5px; height: 6vh;">Laju Napas</div>
+              </b-col>
+              <b-col md="2">
+                <div class="bg-success" style="border-radius: 5px; height: 6vh;">Suhu Tubuh</div>
+              </b-col>
+              <b-col md="2">
+                <div class="bg-warning" style="border-radius: 5px; height: 6vh;">EWS</div>
+              </b-col>
             </b-row>
           </stats-card>
         </b-col>
       </b-row>
       <b-row class="header-h mb-2">
         <b-col md="12">
-          <card type="default" header-classes="bg-transparent" class="header-h p-0">
-            <b-tabs pills class="p-2">
-              <b-tab title="Tab 1" active></b-tab>
-              <b-tab title="Tab 2"></b-tab>
-            </b-tabs>
-          </card>
+          <b-tabs pills justified class="p-0 bg-default p-2" style="border-radius: 5px">
+            <b-tab class="bg-dark" title="Pengkajian Awal" active></b-tab>
+            <b-tab class="bg-dark" title="Lab/Radiologi"></b-tab>
+            <b-tab class="bg-dark" title="CPPT Ranap"></b-tab>
+            <b-tab class="bg-dark" title="Informed Consent"></b-tab>
+            <b-tab class="bg-dark" title="Skrining"></b-tab>
+            <b-tab class="bg-dark" title="Rekonsiliasi Obat"></b-tab>
+            <b-tab class="bg-dark" title="Resume Medis"></b-tab>
+          </b-tabs>
         </b-col>
       </b-row>
     </base-header>
@@ -126,38 +148,98 @@
     <b-container fluid class="mt--7">
       <b-row>
         <b-col xl="2" md="4" sm="12">
-          <stats-card title="Riwayat Berobat"
+          <stats-card
             type="dark"
             sub-title=""
             class="mb-4 container-h">
+            <h2 class="text-center">Riwayat Berobat</h2>
+            <div class="d-flex flex-column" style="border-left: 5px solid #B0AAB0;">
+              <div class="w-100 my-4">
+                <h2>25 Okt 2020</h2>
+                <h3>IGD-RANAP</h3>
+              </div>
+              <div class="w-100 my-4">
+                <h2>3 Okt 2020</h2>
+                <h3>RAJAL</h3>
+              </div>
+              <div class="w-100 my-4">
+                <h2>4 Sept 2020</h2>
+                <h3>RAJAL</h3>
+              </div>
+              <div class="w-100 my-4">
+                <h2>20 Jun 2020</h2>
+                <h3>IGD</h3>
+              </div>
+              <div class="w-100 my-4">
+                <h2>1 Feb 2020</h2>
+                <h3>IGD-RANAP</h3>
+              </div>
+            </div>
           </stats-card>
         </b-col>
         <b-col xl="10" md="8" sm="12">
           <card type="default" header-classes="bg-transparent" class="mb-4 container-h">
             <b-row class="h-100">
               <b-col xl="2" md="4" sm="12" class="h-100">
-                <stats-card title=""
+                <stats-card
                   type="dark"
-                  sub-title=""
-                  class="mb-4 h-100">
-                  <b-card no-body>
-                    <b-tabs pills card vertical>
-                      <b-tab title="Tab 1" active><b-card-text>Tab contents 1</b-card-text></b-tab>
-                      <b-tab title="Tab 2"><b-card-text>Tab contents 2</b-card-text></b-tab>
-                      <b-tab title="Tab 3"><b-card-text>Tab contents 3</b-card-text></b-tab>
+                  class="h-100 w-100">
+                  <b-card no-body class="w-100" style="overflow-y: auto; overflow-x: hidden; height: 53vh;">
+                    <b-tabs pills card vertical fill justified nav-wrapper-class="p-0 w-100 tab-list">
+                      <b-tab title="Anamnesis" active></b-tab>
+                      <b-tab title="Riw. Alergi"></b-tab>
+                      <b-tab title="Status Nyeri"></b-tab>
+                      <b-tab title="Fungsional"></b-tab>
+                      <b-tab title="Resiko Jatuh"></b-tab>
+                      <b-tab title="Skrining Gizi"></b-tab>
+                      <b-tab title="Edukasi"></b-tab>
+                      <b-tab title="Pem. Fisik"></b-tab>
+                      <b-tab title="Lab"></b-tab>
+                      <b-tab title="Radiologi"></b-tab>
+                      <b-tab title="Daf. Masalah"></b-tab>
+                      <b-tab title="Tatalaksana"></b-tab>
+                      <b-tab title="Riw. Obat"></b-tab>
                     </b-tabs>
                   </b-card>
                 </stats-card>
               </b-col>
               <b-col xl="10" md="8" sm="12" class="h-100">
-                <stats-card title="Pengkajian Awal Pasien Rawat Inap"
+                <stats-card
                   type="dark"
-                  sub-title=""
-                  class="mb-4 h-100">
-                  <template slot="footer">
-                    <span class="text-success mr-2">Keluhan Utama</span>
-                    <span class="text-nowrap">Riwayat Penyakit Sekarang</span>
-                  </template>
+                  class="mb-4 h-100 p-5 white-bg">
+                    <h2 class="mb-3 text-center text-uppercase">Pengkajian Awal Pasien Rawat Inap</h2>
+                    <b-row>
+                      <b-col md="6" sm="12">
+                        Autoanamnesis
+                      </b-col>
+                      <b-col md="6" sm="12">
+                        Tanggal Masuk Bangsal: <u>Senin, 26 Okt 2020, 12.00 WIB</u>
+                      </b-col>
+                    </b-row>
+                    <b-row>
+                      <b-col md="6" sm="12">
+                        <u>Alloanamnesis: Tn. Rizal (Suami)</u>
+                      </b-col>
+                      <b-col md="6" sm="12">
+                        Anamnesis oleh: <u>dr. Sukma Ayu</u>
+                      </b-col>
+                    </b-row>
+                    <b-card class="border-dark p-2 my-1 mt-3 white-bg">
+                      <b class="text-uppercase">Keluhan Utama: </b>
+                      Muntah darah sejak 1 hari yang lalu.
+                    </b-card>
+                    <b-card class="border-dark p-2 my-1 white-bg">
+                      <b class="text-uppercase">Riwayat Penyakit Sekarang: </b>
+                      Muntah darah sejak 1 hari yang lalu.
+                    </b-card>
+                    <b-card class="border-dark p-2 my-1 white-bg">
+                      <b class="text-uppercase">Riwayat Penyakit Dahulu: </b>
+                      Muntah darah sejak 1 hari yang lalu.
+                    </b-card>
+                    <b-card class="border-dark p-2 my-1 white-bg">
+                      <b class="text-uppercase">Riwayat Penyakit Keluarga: </b>
+                      Muntah darah sejak 1 hari yang lalu.
+                    </b-card>
                 </stats-card>
               </b-col>
             </b-row>
@@ -165,50 +247,7 @@
         </b-col>
       </b-row>
     </b-container>
-
-    <!--Charts-->
-    <!-- <b-container fluid class="mt--7">
-      <b-row>
-        <b-col xl="8" class="mb-5 mb-xl-0">
-          <card type="default" header-classes="bg-transparent">
-            <b-row align-v="center" slot="header">
-              <b-col>
-                <h6 class="text-light text-uppercase ls-1 mb-1">Overview</h6>
-                <h5 class="h3 text-white mb-0">Sales value</h5>
-              </b-col>
-              <b-col>
-                <b-nav class="nav-pills justify-content-end">
-                </b-nav>
-              </b-col>
-            </b-row>
-          </card>
-        </b-col>
-
-        <b-col xl="4" class="mb-5 mb-xl-0">
-          <card header-classes="bg-transparent">
-            <b-row align-v="center" slot="header">
-              <b-col>
-                <h6 class="text-uppercase text-muted ls-1 mb-1">Performance</h6>
-                <h5 class="h3 mb-0">Total orders</h5>
-              </b-col>
-            </b-row>
-          </card>
-        </b-col>
-      </b-row> -->
-      <!-- End charts-->
-
-      <!--Tables-->
-      <!-- <b-row class="mt-5">
-        <b-col xl="8" class="mb-5 mb-xl-0">
-          <page-visits-table></page-visits-table>
-        </b-col>
-        <b-col xl="4" class="mb-5 mb-xl-0">
-          <social-traffic-table></social-traffic-table>
-        </b-col>
-      </b-row> -->
-      <!--End tables-->
-    <!-- </b-container> -->
-
+  </div>
   </div>
 </template>
 <script>
@@ -284,6 +323,10 @@
   padding-right: 0px;
 }
 
+.card {
+  background-color: #D9E3F1;
+}
+
 .card-stats .card-body {
     padding: 0;
 }
@@ -298,5 +341,37 @@
 
 .container-h {
     height: 60vh;
+}
+
+.tab-list {
+  padding: 0;
+}
+
+/* .nav-pills {
+  background-color: black;
+} */
+
+.nav-link {
+  background: #7F7F7F !important;
+  color: #172B4D !important;
+  font-size: 0.9rem !important;
+  width: 100%;
+  margin: 3px;
+}
+
+.nav-link.active {
+  background: #F9F544 !important;
+  color: #172B4D !important;
+  font-size: 0.9rem !important;
+  width: 100%;
+  margin: 3px;
+}
+
+.card-header {
+  padding: 0 !important;
+}
+
+.white-bg {
+  background-color: #ffffff !important;
 }
 </style>
